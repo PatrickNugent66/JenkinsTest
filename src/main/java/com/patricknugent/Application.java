a8c5a0a2454c31e12adb77848aba921d5cfc5084
PatrickNugent66 <D00218208@student.dkit.ie>
2022-07-07 17:00:17 +0100

    update

--- src/main/java/com/patricknugent/Application.java
+++ src/main/java/com/patricknugent/Application.java
@@ -1,9 +1,5 @@
 package com.patricknugent;
 
-import com.sun.javafx.logging.Logger;
-
-import javax.annotation.PostConstruct;
-
 public class Application {
 
     //Logger logger=LoggerFactory.getLogger(Application.class);
