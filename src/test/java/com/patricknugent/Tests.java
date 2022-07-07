1f45c58cf0d0f6780d7971fe43ee979597c13025
PatrickNugent66 <D00218208@student.dkit.ie>
2022-07-07 16:30:03 +0100

    Update

---
+++ src/test/java/com/patricknugent/Tests.java
@@ -0,0 +1,12 @@
+package com.patricknugent;
+
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+
+public class Tests {
+
+    @Test
+    public void contextLoads() {
+        assertEquals(true, true);
+    }
+}
